de45ca3d9a3d7feb439761ca5ebb34b5e124a7e9
Arkhane Sagunasingha <arkhane@okstate.edu>
2018-01-23 21:37:18 -0600

    Arkhane
    Homework 01

---
+++ CIS2013_Week01_Homework1/Business_card.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	cout << "==============================================================" << "\n";
+	cout << "Arkhane Sagunasingha" << "\n";
+	cout << "Student, Oklahoma State University - Oklahoma City" << "\n";
+	cout << "1304 Sovereign Row                 p: (405) 367-9425" << "\n";
+	cout << "Oklahoma City, OK                  e: arkhane@okstate.edu" << "\n";
+	cout << "                                   g: github.com/arkhane" << "\n";
+	cout << "==============================================================";
+
+	return 0;
+}
